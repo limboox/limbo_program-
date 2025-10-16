b781dcc42335dc58aac4bad1b0ee2b4926973c42
limboox <saulsxb0@gmail.com>
2025-10-16 08:04:06 -0500

    salvador bonifacio Saul

--- Limbo_program/Clase 2/index.py
+++
@@ -1,7 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):
-    page.window.width = 500
-    page.window.height =
-
-ft.app(target=main)
--- Limbo_program/clase 3/index.py
+++
@@ -1,7 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):
-    page.title = "segunda clasee, programacion concurrente "
-    page.add(ft.Text("Welcome to Flet!"))
-
-ft.app(target=main)
